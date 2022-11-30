63cf253b346fec46c8cc89120124b3bf42ae3650
Bhumicka Bhaangaley <bhumickabhaangaley@gmail.com>
2022-11-30 16:30:28 +0530

    added styles

--- app.js
+++ app.js
@@ -4,28 +4,35 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname+"/index.html");
+  res.render("index");
 })
 
 app.post("/", function(req, res) {
   const query = req.body.cityName;
   const apiKey = "e00020a6a5b25e912e41a3e4d63137c6";
   const unit = "metric";
-  const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&units="+unit+"&appid=" + apiKey;
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + unit + "&appid=" + apiKey;
+
+
   https.get(url, function(response) {
 
-
     response.on("data", function(data) {
+
       const weatherData = JSON.parse(data);
+
       const temp = weatherData.main.temp;
-      // console.log(temp);
-      const weatherDescription = weatherData.weather[0].description;
-      // console.log(weatherDescription);
-      const icon = weatherData.weather[0].icon;
-      const imageURL = "http://openweathermap.org/img/wn/" + icon + ".png";
+      var weatherDescription = weatherData.weather[0].description;
+      weatherDescription = weatherDescription.charAt(0).toUpperCase() + weatherDescription.slice(1);
+      // const icon = weatherData.weather[0].icon;
+      // const imageURL = "http://openweathermap.org/img/wn/" + icon + ".png";
       const feelsLike = weatherData.main.feels_like;
       const minTemp = weatherData.main.temp_min;
       const maxTemp = weatherData.main.temp_max;
@@ -34,16 +41,24 @@ app.post("/", function(req, res) {
       const windSpeed = weatherData.wind.speed;
       const cloudiness = weatherData.clouds.all;
 
-      res.write("<p>The temperature in "+query+" is "+temp+"   | "+minTemp+" to "+maxTemp+".</p>");
-      res.write('<img src=' + imageURL+">");
-      res.write("<p>Feels like "+feelsLike+".</p>");
-      res.write(weatherDescription);
-      res.write("<p>Pressure: "+pressure+"hPa</p>");
-      res.write("<p>Humidity: "+humidity+"%</p>");
-      res.write("<p>Wind Speed: "+windSpeed+"m/s</p>");
-      res.write("<p>Cloudiness: "+cloudiness+"%</p>");
+      if (res.statusCode == 200) {
+        res.render("result", {
+          query: query,
+          temp: temp,
+          weatherDescription: weatherDescription,
+          minTemp: minTemp,
+          maxTemp: maxTemp,
+          feelsLike: feelsLike,
+          pressure: pressure,
+          humidity: humidity,
+          windSpeed: windSpeed,
+          cloudiness: cloudiness
 
-      res.send();
+        });
+        
+      }
+
+      // res.send();
 
     })
   })
